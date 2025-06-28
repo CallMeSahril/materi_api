8cbd07040d6b8f5f306666cab091bfc4ec388420
Sahril <103849124+CallMeSahril@users.noreply.github.com>
2025-06-28 17:34:18 +0700

    .

--- routes/map_route.py
+++ routes/map_route.py
@@ -72,3 +72,11 @@ class MapList(Resource):
         cursor.close()
         conn.close()
         return levels
+import random
+
+def fisher_yates_shuffle(arr):
+    n = len(arr)
+    for i in range(n - 1, 0, -1):
+        j = random.randint(0, i)
+        arr[i], arr[j] = arr[j], arr[i]
+    return arr
