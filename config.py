8974d073518177298cb3857dc9c17cb992d1d8a8
Sahril <103849124+CallMeSahril@users.noreply.github.com>
2025-07-16 17:32:51 +0700

    add config

--- config.py
+++ config.py
@@ -1,7 +1,8 @@
 DB_CONFIG = {
     'host': '127.0.0.1',
-    'user': 'root',
-    'password': '',
+    'user': 'flask_user',
+    'password': 'passwordku123',
     'database': 'quis_eng_db'
 }
+
 UPLOAD_FOLDER = 'uploads/'
